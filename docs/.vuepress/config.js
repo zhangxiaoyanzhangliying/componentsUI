6d4a7175aa66fbb4dc657854450fbe38769b8b16
v-zhang.xiaoyan@v-zhang.xiaoyan <v-zhang.xiaoyan@hellogroup.com>
2023-12-18 17:12:08 +0800

    save

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,9 +1,21 @@
-module.exports={
-  base:'/componentsUI/',
-  themeConfig:{
-    sidebar:[
-      "/",
-      "componentDocs/CardComponent",
-    ]
-  }
+module.exports = {
+  title: 'components-UI',
+  base: '/componentsUI/',
+  themeConfig: {
+    nav: [
+      {
+        text: 'Home',
+        link: '/',
+      },
+      {
+        text: 'Github',
+        link: 'https://github.com/zhangxiaoyanzhangliying/componentsUI',
+      },
+      {
+        text: 'Npm',
+        link: 'https://www.npmjs.com/package/zly_components',
+      },
+    ],
+    sidebar: ['/', 'componentDocs/CardComponent'],
+  },
 }
